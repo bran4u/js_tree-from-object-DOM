1e7e36786d14d8e971ce1158c8b46679ed5ab296
bran4u <s.oselskiy@gmail.com>
2020-06-16 09:24:46 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const list = document.createElement('ul');
+
+  element.appendChild(list);
+
+  for (const item in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = item;
+    list.appendChild(listItem);
+
+    if (Object.values(data[item])) {
+      createTree(listItem, data[item]);
+    };
+  };
+}
+
+createTree(tree, food);
